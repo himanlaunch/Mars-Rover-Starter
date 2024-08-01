f3eed0da69f57777dae7fff7d376dfa7242083c0
himanlaunch <himanshureddyk1995@gmail.com>
2024-07-31 23:25:14 -0500

    .json

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -9,4 +9,14 @@ describe("Command class", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
+  it("constructor sets command type", function() {
+    let command = new Command('MODE_CHANGE');
+    expect(command.commandType).toEqual('MODE_CHANGE');
+  });
+
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    let command = new Command('MOVE', 12000);
+    expect(command.value).toEqual(12000);
+  });
+  
 });
